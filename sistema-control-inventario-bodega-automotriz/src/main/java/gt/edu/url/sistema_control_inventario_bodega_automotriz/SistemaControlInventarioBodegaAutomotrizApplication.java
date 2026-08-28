2dbf3a4cea3a2445d72b24d7240486eca979d2dd
vicsit4213 <VJSITAMUL@CORREO.URL.EDU.GT>
2026-08-27 20:49:47 -0600

    Paquete Java sprint
    Se cambio el puerto a 8081

---
+++ sistema-control-inventario-bodega-automotriz/src/main/java/gt/edu/url/sistema_control_inventario_bodega_automotriz/SistemaControlInventarioBodegaAutomotrizApplication.java
@@ -0,0 +1,13 @@
+package gt.edu.url.sistema_control_inventario_bodega_automotriz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaControlInventarioBodegaAutomotrizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaControlInventarioBodegaAutomotrizApplication.class, args);
+	}
+
+}
